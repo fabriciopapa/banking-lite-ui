5e5efa7167e02232e7eed2349de7124b24595e25
Fabricio Papa <FabricioP@infodom.icorp.com.uy>
2017-09-14 14:26:26 -0300

    imagenes y se completan animaciones

---
+++ Banking.Lite.UI/src/app/components/components_exports.js
@@ -0,0 +1,6 @@
+"use strict";
+var menu_button_component_1 = require("./menu_button/menu_button_component");
+exports.MenuButtonComponent = menu_button_component_1.MenuButtonComponent;
+var menu_component_1 = require("./menu/menu_component");
+exports.MenuComponent = menu_component_1.MenuComponent;
+//# sourceMappingURL=components_exports.js.map
